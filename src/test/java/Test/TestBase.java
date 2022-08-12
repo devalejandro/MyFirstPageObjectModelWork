3c008e5ca511222546485c91deabce400ac8018e
Alejandro <dev.alejandrolescano@gmail.com>
2022-08-12 01:42:09 -0300

    Configuracion de framework

---
+++ src/test/java/Test/TestBase.java
@@ -0,0 +1,21 @@
+package Test;
+
+import org.testng.annotations.*;
+
+public class TestBase {
+/*
+    @BeforeSuite
+
+    @BeforeClass
+
+    @BeforeMethod
+
+    @BeforeTest
+
+    @Test
+
+    @AfterTest
+
+    @AfterMethod
+*/
+}
